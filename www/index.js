c60c4bf26dd2c01cc90a98130f519389c4565dde
mostafaei2002 <sinamostafaei2002@gmail.com>
2024-04-27 17:27:36 +0330

    fix frontend

--- www/index.js
+++ www/index.js
@@ -2,7 +2,20 @@ import * as sim from 'lib-simulation-wasm';
 
 const simulation = new sim.Simulation();
 const viewport = document.getElementById('viewport');
+const viewportWidth = viewport.width;
+const viewportHeight = viewport.height;
+
+const viewportScale = window.devicePixelRatio || 1;
+
+viewport.width = viewportWidth * viewportScale;
+viewport.height = viewportHeight * viewportScale;
+
+viewport.style.width = viewportWidth + 'px';
+viewport.style.height = viewportHeight + 'px';
+
 const ctxt = viewport.getContext('2d');
+
+ctxt.scale(viewportScale, viewportScale);
 
 CanvasRenderingContext2D.prototype.drawTriangle = function (
   x,
@@ -17,14 +30,15 @@ CanvasRenderingContext2D.prototype.drawTriangle = function (
     y + Math.cos(rotation) * size * 1.5
   );
 
-  this.lineTo(
-    x - Math.sin(rotation + 2.0 / 3.0 * Math.PI) * size,
-    y + Math.cos(rotation + 2.0 / 3.0 * Math.PI) * size,
-);
+  this.moveTo(
+    x - Math.sin(rotation + (2.0 / 3.0) * Math.PI) * size,
+    y + Math.cos(rotation + (2.0 / 3.0) * Math.PI) * size
+  );
 
-this.lineTo(
-    x - Math.sin(rotation + 4.0 / 3.0 * Math.PI) * size,
-    y + Math.cos(rotation + 4.0 / 3.0 * Math.PI) * size,
+  this.lineTo(
+    x - Math.sin(rotation + (4.0 / 3.0) * Math.PI) * size,
+    y + Math.cos(rotation + (4.0 / 3.0) * Math.PI) * size
+  );
 
   this.lineTo(
     x - Math.sin(rotation) * size * 1.5,
